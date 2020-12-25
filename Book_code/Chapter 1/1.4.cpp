674de43cd43d8af7d08447b10f81a0333f5e610c
Wellington Silva <wefinalfantasy@gmail.com>
2020-12-25 11:32:41 -0300

    Create 1.4

---
+++ Book_code/Chapter 1/1.4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    bool // Boolean, possible values are true and false
+
+    char // character, for example, 'a', 'z', and '9'
+
+    int // integer, for example, -273, 42, and 1066
+
+    double // double-precision floating-point number, for example, -273.15, 3.14, and 6.626e-34
+
+    unsigned // non-negative integer, for example, 0, 1, and 999 (use for bitwise logical operations)
+}
