293259d1bf8665c5dc93edbc0853b2d5c039e6a6
Wellington Silva <wellington.71319@gmail.com>
2021-01-17 10:48:13 -0300

    Create 2.3

---
+++ Book_code/Chapter 2/2.3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Vector {
+public:
+    Vector(int s) :elem{new double[s]}, sz{s} {}    // construct a Vector
+    double& operator[](int i) { return elem[i]; }   // element access: subscripting
+    int size() { return sz; }
+private:
+    double* elem;           // pointer to the elements
+    int sz;                 // the number of elements
+};
+
+double read_and_sum(int s) {
+    Vector v(s);                        // make a vector of s elements
+    for (int i=0; i!=v.size(); ++i)
+        std::cin >> v[i];               // read into elements
+    
+    double sum = 0;
+    for (int i=0; i!=v.size(); ++i)
+        sum+=v[i];                      // take the sum of the elements
+    return sum;
+}
+
+int main() {
+    // Classes -> membros públicos e privados
+    // Struc   -> all públic
+
+    Vector v(6);        // a Vector with 6 elements
+
+    double sum = read_and_sum(4);
+    std::cout << sum << std::endl;
+}
