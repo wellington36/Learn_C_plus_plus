a559b4d17b1a17db3a29c9b6abd14dbbed37f7cf
Wellington Silva <wellington.71319@gmail.com>
2021-01-26 09:42:06 -0300

    Create Chapter 3

---
+++ Book_code/Chapter 3/3.2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+// #include "Vector.h"      // other file
+
+// Vector.h
+
+class Vector {
+    public:
+        Vector(int s);
+        double& operator[](int i);
+        int size();
+    private:
+        double* elem:
+        int sz;
+};
+
+double sqrt_sum(Vector& v) {
+    double sum = 0;
+
+    for (int i = 0; i!=v.size(); ++i) {
+        sum+=std::sqrt(v[i]);
+    }
+    return sum;
+}
+
+int main() {
+    // Separate Compilation
+}
